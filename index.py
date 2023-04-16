9ddbe05099160ea59eff82f4a2de3ae28d8062e8
“YuQiao0428” <leah.huang0428@gmail.com>
2023-04-16 15:23:32 +0800

    make it better

--- index.py
+++ index.py
@@ -2,16 +2,19 @@ from flask import Flask
 app = Flask(__name__)
 import openai
 import os
-openai.api_key = os.getenv("OPENAI_API_KEY")
-@app.route("/")
-def index():
+openai.api_key = os.getenv("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")
+@app.route("/webhook", methods=["POST"])
+def webhook():
+    req = request.get_json(force=True)
+    msg =  req["queryResult"]["queryText"]
     response = openai.Completion.create(
         model="text-davinci-003",
-        prompt="靜宜大學資管系特色？",
-        max_tokens=128,
+        prompt=msg,
+        max_tokens=100,
         temperature=0.5,
     )
-    msg = response.choices[0].text
-    return msg
+    info = "AI：" + response.choices[0].text
+    return make_response(jsonify({"fulfillmentText": info}))
+
 if __name__ == "__main__":
     app.run()
